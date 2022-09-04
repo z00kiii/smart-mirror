3dfc9db390f5c7470b1bad4d790fbd66dbfcf760
Atash <atash.hauschild@web.de>
2022-09-04 17:29:57 +0200

    startup revert

--- smart-mirror-react/public/main.js
+++ smart-mirror-react/public/main.js
@@ -1,4 +1,19 @@
 const { app, BrowserWindow } = require("electron");
+const axios = require("axios");
+
+const winurl = "http://localhost:3000";
+let interval = null
+
+const startApp = (win) => {
+  axios.get(winurl, { timeout: 1000 }).then((res) => {
+    if (res.status === 200) {
+      if(interval){
+        clearInterval(interval);
+      }
+      win.loadURL(winurl);
+    }
+  });
+}
 
 function createWindow() {
   // Create the browser window.
@@ -12,7 +27,10 @@ function createWindow() {
   //win.webContents.openDevTools();
 
   //load the index.html from a url
-  win.loadURL("http://localhost:3000");
+  startApp(win);
+  interval = setInterval(() => {
+    startApp(win);
+  }, 5000);
 }
 
 //app.commandLine.appendSwitch("trace-warnings");
--- smart-mirror-react/src/App.js
+++ smart-mirror-react/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Spotify from "./components/Spotify/Spotify";
 import Controlbar from "./components/Controlbar/Controlbar";
 import Date from "./components/Date/Date";
@@ -7,7 +7,6 @@ import Weather from "./components/Weather/Weather";
 import useAuth from "./components/Spotify/useAuth";
 import SpotifyWebApi from "spotify-web-api-node";
 import { clientId, loginUrl } from "./components/Spotify/spotifyData";
-import axios from "axios";
 
 const spotifyApi = new SpotifyWebApi({
   clientId: clientId,
@@ -20,39 +19,16 @@ function App() {
   }
 
   const accessToken = useAuth(code);
-  const [showSpotify, setShowSpotify] = useState(false);
-  let interval = null;
 
   useEffect(() => {
     if (!accessToken) return;
     spotifyApi.setAccessToken(accessToken);
   }, [accessToken]);
 
-  const startApp = () => {
-    axios.get("http://localhost:3000", { timeout: 1000 }).then((res) => {
-      if (res.status === 200) {
-        if (interval) {
-          clearInterval(interval);
-        }
-        setShowSpotify(true);
-      }
-    });
-  };
-
-  startApp();
-  interval = setInterval(() => {
-    console.log("asking");
-    startApp();
-  }, 5000);
-
   return (
     <div className="bg-black text-white h-screen border-2 border-gray-500 p-2 tracking-wide font-mono main">
       <div className="w-32 absolute bottom-0.5 left-1 m-2 mb-1.5 leading-tight">
-        {showSpotify ? (
-          <Spotify spotifyApi={spotifyApi} />
-        ) : (
-          <div>fuck off</div>
-        )}
+        <Spotify spotifyApi={spotifyApi} />
       </div>
       <div className="w-12 absolute bottom-5 right-1 m-2 leading-tight">
         <Controlbar spotifyApi={spotifyApi} />
